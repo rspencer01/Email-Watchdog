e5ed34768db10da46e3c989efdeb90484a70ecd4
Robert Spencer <general@robertandrewspencer.com>
2020-09-08 17:09:41 +0100

    Use click CLI

---
+++ nimbus/cli.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import datetime
+
+import click
+from fabulous.color import bold
+import gpx_trip
+import yaml
+
+import nimbus.travel
+import nimbus.notifications
+
+config = yaml.full_load(open("config.yaml"))
+
+
+@click.group()
+def travel():
+    """All things to do with my movements."""
+
+
+@click.command()
+def current_position():
+    """Where am I now?"""
+    print(bold("You are at:"))
+    print(bold("==========="))
+    position = nimbus.travel.current_position()
+    print("Place:      {}".format(position["location"]))
+    print("Latitude:   {:.4f}".format(position["lat"]))
+    print("Longitude:  {:.4f}".format(position["lon"]))
+    print("Stationary: {}".format(position["stationary"]))
+
+
+@click.command()
+@click.argument("date", default=None)
+def day(date):
+    """What was yesterday like?
+
+    Due to perculiarities, this only works after midday.
+    TODO(robert) set the correct file so it works whenever.
+    """
+    if date is None:
+        filename = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime(
+            config["tracking"]["historic_data_file"]
+        )
+    else:
+        filename = datetime.datetime.strptime(date, "%Y-%m-%d").strftime(
+            config["tracking"]["historic_data_file"]
+        )
+
+    info = gpx_trip.extract_info(
+        open(filename).read(), predefined_stops=config["places"]
+    )
+    print(" " * 90, end="\r")
+    for trip in info["trips"]:
+        print(bold("Trip"))
+        print(
+            info["stops"][trip["from"]]["short_name"],
+            "-",
+            info["stops"][trip["to"]]["short_name"],
+        )
+        print(
+            "{} - {}".format(
+                trip["start"].strftime("%H:%M"), trip["end"].strftime("%H:%M")
+            )
+        )
+        print()
+    print(info)
+
+
+@click.command()
+def notifications():
+    """Update the notifications"""
+    nimbus.notifications.update()
+
+
+travel.add_command(current_position)
+travel.add_command(day)
---
+++ setup.py
@@ -0,0 +1,29 @@
+"""Setup for nimbus."""
+
+from setuptools import setup
+
+setup(
+    name="nimbus",
+    description="",
+    version="0.0.1dev",
+    author="R. A. Spencer",
+    author_email="general@robertandrewspencer.com",
+    packages=["nimbus"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points='''
+        [console_scripts]
+        nimbus-travel=nimbus.cli:travel
+        nimbus-notifications=nimbus.cli:notifications
+    ''',
+    python_requires=">=3.5",
+    install_requires=[
+        "gpxpy==1.3.5",
+        "pandas==0.25.3",
+        "traces==0.5.2",
+        "sklearn==0.0",
+        "click"
+    ],
+)
